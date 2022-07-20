a44cc574fc4ecbf614bf17610f81a81b83fc81ef
jozefhruska <me@jozefhruska.com>
2022-07-20 09:15:53 +0200

    chore: update @semantic-release/git config

--- dist/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { Session } from 'next-auth';
-import { SWRConfiguration, SWRResponse } from 'swr';
-import { UseSessionOptions as OriginalUseSessionOptions } from 'next-auth/react';
-export interface UseSessionOptions<R extends boolean> extends Partial<OriginalUseSessionOptions<R>> {
-    config?: SWRConfiguration<Session | null>;
-}
-export declare const useSession: <R extends boolean>(options?: UseSessionOptions<R> | undefined) => SWRResponse<Session | null>;
-//# sourceMappingURL=index.d.ts.map
